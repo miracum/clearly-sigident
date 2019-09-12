cc370cfa34c7aed5bf7911701edc02b859b6bc8c
VeyJo <johannes.vey@stud-mail.uni-wuerzburg.de>
2019-09-12 15:03:10 +0200

    adapted gPCA

--- R/batchcorrection.R
+++ R/batchcorrection.R
@@ -1,12 +1,12 @@
 # create design
-createDiagnosisDesign_ <- function(mergeset, controlname, targetname, targetcol){
+createDiagnosisDesign_ <- function(samplemetadata, controlname, targetname, targetcol){
   stopifnot(
     is.character(controlname),
     is.character(targetname),
     is.character(targetcol)
   )
 
-  diag <- as.vector(mergeset[[targetcol]])
+  diag <- as.vector(samplemetadata[[targetcol]])
 
   diagnosis <- gsub(controlname, "0", diag)
   diagnosis <- gsub(targetname, "1", diagnosis)
@@ -16,14 +16,10 @@ createDiagnosisDesign_ <- function(mergeset, controlname, targetname, targetcol)
   return(list(diagnosis = diagnosis, design = design))
 }
 
-# generate data frame with expression values and model matrix regardarding diagnosis
-createDataMatrix_ <- function(mergeset){
-  return(mergeset@assayData$exprs)
-}
-
 # create batch
-createBatch_ <- function(eset1, eset2, eset3){
+createBatch_ <- function(study_length){
   # TODO make this working generically
+  
   return(rep(c(1,2,3), times = c(ncol(eset1),ncol(eset2),ncol(eset3))))
 }
 
@@ -31,6 +27,3 @@ batchCorrection_ <- function(x, batch){
   return(gPCA::gPCA.batchdetect(x = t(x), batch = batch, center = FALSE))
 }
 
-createCombat_ <- function(DF, batch, design){
-  return(sva::ComBat(DF, batch = batch, mod = design, par.prior = T))
-}
