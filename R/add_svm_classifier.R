fc58dde51af3959e7de507064ea9b95295dfc5c0
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-12-09 18:05:49 +0100

    started svm vignette

--- R/add_svm_classifier.R
+++ R/add_svm_classifier.R
@@ -5,12 +5,6 @@ svm_classifier <- function(traininglist, seed) {
   # # initialize gridserach parameters
   # gr_init <- init_grid_search()
 
-  # go parallel
-  ncores <- parallel::detectCores()
-  cores <- ifelse(ncores > 4, 4, ncores)
-  cl <- parallel::makeCluster(cores)
-  doParallel::registerDoParallel(cl)
-
   # perform cv forecasting
   #set.seed(seed)
   #outlist$caret_train <- caret::train(
@@ -24,43 +18,43 @@ svm_classifier <- function(traininglist, seed) {
   #  maxit = 1e7
   #)
 
-  train.svm <- caret::trainControl(method = "repeatedcv", number = 10,
-                                    repeats = 5, savePredictions = TRUE, search = "random")
+  train_svm <- caret::trainControl(
+    method = "repeatedcv", number = 10,
+    repeats = 5, savePredictions = TRUE, search = "random"
+  )
 
   print("step1")
 
-  svm_model <- caret::train(
+  # go parallel
+  ncores <- parallel::detectCores()
+  cores <- ifelse(ncores > 4, 4, ncores)
+  cl <- parallel::makeCluster(cores)
+  doParallel::registerDoParallel(cl)
+
+  outlist$svm_model <- caret::train(
                x = traininglist$train$x,
                y = as.factor(as.factor(traininglist$train$y)),
                method = "svmLinear",
                #family = "binomial",
                #tuneGrid = gr_init$srchGrd,
-               trControl = train.svm, # or train.svm
+               trControl = train_svm, # or train.svm
                preProc = c("center", "scale"),
                tuneLength = 10,
                allowParallel = T)
-
-  print(svm_model)
-
-  print("step2")
-
-  importance.svm <- caret::varImp(svm_model, scale = FALSE)
-  print(importance.svm)
-  plot(importance.svm, top = 20)
-
-  print("step3")
-
-  svm.pred <- predict(svm_model, traininglist$test$x)
-
-  print("predictions:")
-  print(svm.pred)
-  print("test:")
-  print(as.factor(traininglist$test$y))
-
- confusion.svm <- caret::confusionMatrix(svm.pred, as.factor(traininglist$test$y))
- print(confusion.svm)
-
   # stop parallel computation
   parallel::stopCluster(cl)
   gc()
+
+  print(outlist$svm_model)
+
+  outlist$importance_svm <- caret::varImp(outlist$svm_model, scale = FALSE)
+
+  outlist$svm_pred <- predict(outlist$svm_model, traininglist$test$x)
+
+
+ outlist$confusion_svm <- caret::confusionMatrix(
+   outlist$svm_pred, as.factor(traininglist$test$y)
+   )
+
+ return(outlist)
 }
