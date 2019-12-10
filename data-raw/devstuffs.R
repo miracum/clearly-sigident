8efbfec01828a686c0647bafb5cd2c5c095c7276
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-12-10 11:24:55 +0100

    updated development package to point to sigident.preproc development branch; adjusted vignette to not calculate plots during batch correction due to ressource intensity

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -101,7 +101,7 @@ usethis::use_package("knitr", type = "Suggests")
 usethis::use_package("lintr", type = "Suggests")
 
 # Development package
-mytag <- "v0.0.1"
+mytag <- "development"
 devtools::install_git(url = "https://gitlab.miracum.org/clearly/sigident.preproc", ref = mytag, upgrade = "always")
 usethis::use_dev_package("sigident.preproc", type = "Imports")
 #usethis::use_dev_package("DQAstats", type = "Imports")
