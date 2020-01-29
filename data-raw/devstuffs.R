33370c11f5facf80c362ef8bfd75c55d6c2be271
Julian Henke <julianhenke7@t-online.de>
2020-01-29 15:24:29 +0100

    added package kknn to devstuffs

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -81,6 +81,7 @@ usethis::use_package("survival", type="Imports")
 usethis::use_package("plyr", type="Imports")
 usethis::use_package("survminer", type="Imports")
 usethis::use_package("knitr", type="Imports")
+usethis::use_package("kknn", type="Imports")
 
 
 
